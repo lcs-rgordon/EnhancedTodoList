efcf1c2d041a69b76219fb224bd45c98dc4582c0
Russell Gordon <rgordon@lcs.on.ca>
2024-01-18 09:38:40 -0500

    Initial project organization.

--- EnchancedTodoList/EnchancedTodoListApp.swift
+++ EnchancedTodoList/EnchancedTodoListApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct EnchancedTodoListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingView()
         }
     }
 }
---
+++ EnchancedTodoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnchancedTodoList
+//
+//  Created by Russell Gordon on 2024-01-18.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
