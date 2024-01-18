34f3678512ce701dc9ccd97f304f83eb38771fa3
Russell Gordon <rgordon@lcs.on.ca>
2024-01-18 09:35:07 -0500

    Initial Commit

---
+++ EnchancedTodoList/EnchancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnchancedTodoListApp.swift
+//  EnchancedTodoList
+//
+//  Created by Russell Gordon on 2024-01-18.
+//
+
+import SwiftUI
+
+@main
+struct EnchancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
