8dc17dc9a39e28afb132da4fa91004042aec2b4c
Russell Gordon <rgordon@lcs.on.ca>
2024-01-18 09:43:32 -0500

    Added tab view.

---
+++ EnchancedTodoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnchancedTodoList
+//
+//  Created by Russell Gordon on 2024-01-18.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text("Statistics view")
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
---
+++ EnchancedTodoList/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnchancedTodoList
+//
+//  Created by Russell Gordon on 2024-01-18.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text("To-do list view")
+    }
+}
+
+#Preview {
+    TodoListView()
+}
